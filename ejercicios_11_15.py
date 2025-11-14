ecc11748f3c6e4fa0b2d4e79e105c4cdb17ba4bc
Mat <matiasaguirrecorrea@gmail.com>
2025-11-14 16:02:22 -0500

    si

--- ejercicios_11_15.py
+++ ejercicios_11_15.py
@@ -78,4 +78,75 @@ carrito = ["Pan", "Leche", "Huevos"]
 actualizar_carrito(carrito, "agregar", "Manzanas")
 actualizar_carrito(carrito, "agregar", "Café", posicion=1)
 actualizar_carrito(carrito, "quitar", item=None, posicion=0)
-actualizar_carrito(carrito, "quitar", item=None, posicion=len(carrito)-1) """
+actualizar_carrito(carrito, "quitar", item=None, posicion=len(carrito)-1)  """
+
+#14
+
+""" def fusionar_candidatos(base_a, base_b, clave_desc = False, primeros_n = 5):
+    
+    if not base_a or not base_b:
+        print("ERROR: Las listas no pueden estar vacías.")
+    
+    tipo_a = type(base_a[0])
+    tipo_b = type(base_b[0])
+    
+    if tipo_a is not tipo_b:
+        raise TypeError(f"Las listas deben contener el mismo tipo de dato. "
+                        f"Base A: {tipo_a}, Base B: {tipo_b}")
+    
+    fusion = base_a + base_b
+    
+    try:
+        
+        f_ordenada = sorted(fusion, reverse = clave_desc)
+    
+    except Exception:
+        raise ValueError("Los elementos no son comparables para ordenar.")
+    
+    print(f"Primeros {primeros_n} candidatos:")
+    for candidato in f_ordenada[:primeros_n]:
+        print("-", candidato)
+    
+    return f_ordenada
+
+base_a = ["Ana", "Luis", "Carla"]
+base_b = ["Pedro", "Jorge"]
+
+resultado = fusionar_candidatos(base_a, base_b, clave_desc=True, primeros_n=3) """
+
+# 15
+
+""" def depurar_ventas(ventas, umbral):
+    
+    ventas_validas = []
+    removidos = 0
+    
+    for venta in ventas:
+        if venta is None or  venta < 0:
+            removidos += 1
+        else:
+            ventas_validas.append(venta)
+            
+    counter = {}
+    ventas_f = []
+    
+    for venta in ventas_validas:
+        counter.setdefault(venta, 0)
+
+        if counter[venta] < umbral:
+            ventas_f.append(venta)
+            counter[venta] += 1
+        else:
+            removidos += 1
+    
+    print("Lista final depurada:", ventas_f)
+    print("Cantidad de elementos removidos:", removidos)
+
+    return ventas_f, removidos
+
+ventas = [10, 20, 20, 20, 20, None, -5, 10, 10, 10]
+umbral = 2
+
+depurar_ventas(ventas, umbral) """
+    
+    
